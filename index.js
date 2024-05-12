35241d7506ba746dc818d6b45b9f0edc46ea309c
Shrimanashish <av2989001@gmail.com>
2024-05-12 12:37:52 +0530

    changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({extended:true}));
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(cors());
 app.use('/',Routes);
-const Port   = process.env.PORT;
+const Port   = process.env.PORT || 6010;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
